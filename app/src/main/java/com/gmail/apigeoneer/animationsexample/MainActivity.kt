00d7cd8b2bc2b0d84ef9e17f361cea1b7afe3016
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-04-11 13:16:48 +0530

    translater()

--- app/src/main/java/com/gmail/apigeoneer/animationsexample/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/animationsexample/MainActivity.kt
@@ -57,12 +57,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun rotater() {
-        val animator = ObjectAnimator.ofFloat(ivStar, View.ROTATION, -360f, 0f)
-        // Change the animation duration to 1000 ms (since 3000 ms i.e. the default feels too short here)
-        animator.duration = 900
-        // to avoid jank
-        animator.addListener(object: AnimatorListenerAdapter() {
+    private fun disableDuringAnimation(view: View, animator: ObjectAnimator?) {
+        animator?.addListener(object: AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator?) {
                 btnRotate.isEnabled = false
             }
@@ -71,12 +67,36 @@ class MainActivity : AppCompatActivity() {
                 btnRotate.isEnabled = true
             }
         })
+    }
+
+    private fun rotater() {
+        val animator = ObjectAnimator.ofFloat(ivStar, View.ROTATION, -360f, 0f)
+        // Change the animation duration to 1000 ms (since 3000 ms i.e. the default feels too short here)
+        animator.duration = 900
+        // to avoid jank
+        disableDuringAnimation(btnRotate, animator)
         // run the animation
         animator.start()
     }
 
     private fun translater() {
-        TODO("Not yet implemented")
+        /**
+         *  200 f -> not the distance to be travelled, it is the end position
+         *  START POSITION -> the default position of the star
+         *
+         *  Problem 1:  the animation is only being set up to run one way; it animates the star 200 pixels to the right… and that’s it.
+         *              So if we want it to come back, we’re going to need something extra.
+         *  Problem 2:  subsequent animations don’t appear to do anything because the animation is set up to run to a value of 200.
+         *              After the animation has run, the value is already at 200, so there’s no place else to go.
+         */
+        val animator = ObjectAnimator.ofFloat(ivStar, View.TRANSLATION_X, 200f)
+        /**
+         * Solution to prob.s 1 & 2 :  REPETITION
+         */
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        disableDuringAnimation(btnTranslate, animator)
+        animator.start()
     }
 
     private fun sacler() {
